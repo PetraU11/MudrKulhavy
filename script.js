22a55879e2cb0499281effd7b92a8af009750b33
Petra Ulicna <petra.ulicna@hotmail.com>
2024-01-01 22:11:12 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const hamburger = document.querySelector(".header__hbg-wrapper")
 const hamburgerMenu = document.querySelector(".header__nav")
 
+
 hamburger.addEventListener("click",()=>{
     hamburgerMenu.style.display = "block";
-
-
+    hamburgerMenu.classList.add("shown");
 })
