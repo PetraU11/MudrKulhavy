d548c950cc581869e919cfd0e80fc2fe2c59d2c1
Petra Ulicna <petra.ulicna@hotmail.com>
2024-01-03 21:32:43 +0100

    test

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const hamburger = document.querySelector(".header__hbg-wrapper")
 const hamburgerMenu = document.querySelector(".header__nav")
+const hamburgerBtn = document.querySelectorAll(".header__nav-button")
 
 
-hamburger.addEventListener("click",()=>{
-    hamburgerMenu.style.display = "block";
-    hamburgerMenu.classList.add("shown");
-})
+hamburger.addEventListener("click", () => {
+   
+    if (hamburgerMenu.classList.contains("shown")) {
+        hamburgerMenu.style.display = "none";
+        hamburgerMenu.classList.remove("shown");
+        hamburgerBtn.forEach((button) => {
+            button.classList.remove("shown-btn");
+        });
+    } else {
+        hamburgerMenu.style.display = "block";
+        hamburgerMenu.classList.add("shown");
+        hamburgerBtn.forEach((button) => {
+            button.classList.add("shown-btn");
+        });
+    }
+});
